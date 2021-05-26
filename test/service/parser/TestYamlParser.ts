ecbc828b2f506c812ec3d072b02948a8fbe348dd
ssube <seansube@gmail.com>
2021-05-26 00:24:18 -0500

    fix(test): cover YAML parser

--- test/service/parser/TestYamlParser.ts
+++ test/service/parser/TestYamlParser.ts
@@ -1,4 +1,42 @@
+import { expect } from 'chai';
+import { Container, NullLogger } from 'noicejs';
+
+import { LocalModule } from '../../../src/module/LocalModule';
+import { YamlParser } from '../../../src/service/parser/YamlParser';
+
 describe('yaml parser', () => {
-  xit('should load and validate data files from string');
-  xit('should save data files to string');
+  it('should load and validate data files from string', async () => {
+    const container = Container.from(new LocalModule());
+    await container.configure({
+      logger: NullLogger.global,
+    });
+
+    const parser = await container.create(YamlParser);
+    const data = parser.load(`
+states: []
+worlds: []
+    `);
+
+    expect(data).to.deep.equal({
+      states: [],
+      worlds: [],
+    });
+  });
+
+  it('should save data files to string', async () => {
+    const container = Container.from(new LocalModule());
+    await container.configure({
+      logger: NullLogger.global,
+    });
+
+    const parser = await container.create(YamlParser);
+    const data = parser.save({
+      states: [],
+      worlds: [],
+    });
+
+    expect(data).to.deep.equal(`states: []
+worlds: []
+`);
+  });
 });

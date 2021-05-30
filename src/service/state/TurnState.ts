080a2f29bb34bc95f6934351c165bba109cec9fa
ssube <seansube@gmail.com>
2021-05-30 16:17:34 -0500

    fix(command): fix scrolling for debug meta-command

--- src/service/state/TurnState.ts
+++ src/service/state/TurnState.ts
@@ -313,12 +313,15 @@ export class LocalStateService implements StateService {
 
   public async doDebug(): Promise<void> {
     const state = await this.save();
-    const line = debugState(state).join('\n');
-    this.event.emit(EVENT_STATE_OUTPUT, {
-      line,
-      step: state.step,
-      volume: ShowVolume.WORLD,
-    });
+    const lines = debugState(state);
+
+    for (const line of lines) {
+      this.event.emit(EVENT_STATE_OUTPUT, {
+        line,
+        step: state.step,
+        volume: ShowVolume.WORLD,
+      });
+    }
   }
 
   public async doGraph(path: string): Promise<void> {

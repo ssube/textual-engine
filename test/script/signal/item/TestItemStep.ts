0f955c50d299fc0a81679799875c0b5e81536fd0
ssube <seansube@gmail.com>
2021-06-10 00:08:10 -0500

    fix(script): use typed errors for item and room step, test

---
+++ test/script/signal/item/TestItemStep.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import { NullLogger } from 'noicejs';
+import { createStubInstance } from 'sinon';
+
+import { ScriptTargetError } from '../../../../src/error/ScriptTargetError';
+import { SignalItemStep } from '../../../../src/script/signal/item/ItemStep';
+import { MathRandomGenerator } from '../../../../src/service/random/MathRandom';
+import { LocalScriptService } from '../../../../src/service/script/LocalScript';
+import { VERB_WAIT } from '../../../../src/util/constants';
+import { makeTestActor, makeTestRoom } from '../../../entity';
+import { getStubHelper } from '../../../helper';
+import { testTransfer } from '../../helper';
+
+describe('item step scripts', () => {
+  describe('item step command', () => {
+    it('should require the script target be an item', async () => {
+      const script = createStubInstance(LocalScriptService);
+      const stateHelper = getStubHelper();
+      const transfer = testTransfer();
+
+      const context = {
+        command: {
+          index: 0,
+          input: '',
+          target: '',
+          verb: VERB_WAIT,
+        },
+        data: new Map(),
+        logger: NullLogger.global,
+        random: createStubInstance(MathRandomGenerator),
+        room: makeTestRoom('', '', '', [], []),
+        script,
+        state: stateHelper,
+        transfer,
+      };
+
+      await expect(SignalItemStep.call(makeTestActor('', '', ''), context)).to.eventually.be.rejectedWith(ScriptTargetError);
+      await expect(SignalItemStep.call(makeTestRoom('', '', '', [], []), context)).to.eventually.be.rejectedWith(ScriptTargetError);
+    });
+  });
+});
---
+++ test/script/signal/room/TestRoomStep.ts
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import { NullLogger } from 'noicejs';
+import { createStubInstance } from 'sinon';
+
+import { ScriptTargetError } from '../../../../src/error/ScriptTargetError';
+import { SignalRoomStep } from '../../../../src/script/signal/room/RoomStep';
+import { MathRandomGenerator } from '../../../../src/service/random/MathRandom';
+import { LocalScriptService } from '../../../../src/service/script/LocalScript';
+import { VERB_WAIT } from '../../../../src/util/constants';
+import { makeTestActor, makeTestItem, makeTestRoom } from '../../../entity';
+import { getStubHelper } from '../../../helper';
+import { testTransfer } from '../../helper';
+
+describe('room step scripts', () => {
+  describe('room step command', () => {
+    it('should require the script target be an item', async () => {
+      const script = createStubInstance(LocalScriptService);
+      const stateHelper = getStubHelper();
+      const transfer = testTransfer();
+
+      const context = {
+        command: {
+          index: 0,
+          input: '',
+          target: '',
+          verb: VERB_WAIT,
+        },
+        data: new Map(),
+        logger: NullLogger.global,
+        random: createStubInstance(MathRandomGenerator),
+        room: makeTestRoom('', '', '', [], []),
+        script,
+        state: stateHelper,
+        transfer,
+      };
+
+      await expect(SignalRoomStep.call(makeTestActor('', '', ''), context)).to.eventually.be.rejectedWith(ScriptTargetError);
+      await expect(SignalRoomStep.call(makeTestItem('', '', ''), context)).to.eventually.be.rejectedWith(ScriptTargetError);
+    });
+  });
+});

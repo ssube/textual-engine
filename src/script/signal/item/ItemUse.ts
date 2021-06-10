cfe19e86c802a6f3dc93626779acfe296673400f
ssube <seansube@gmail.com>
2021-06-10 00:26:54 -0500

    fix(script): throw typed error from item use script, test

--- src/script/signal/item/ItemUse.ts
+++ src/script/signal/item/ItemUse.ts
@@ -1,8 +1,14 @@
 import { mustExist } from '@apextoaster/js-utils';
 
+import { ScriptTargetError } from '../../../error/ScriptTargetError';
+import { isItem } from '../../../model/entity/Item';
 import { ScriptContext, ScriptTarget } from '../../../service/script';
 
 export async function SignalItemUse(this: ScriptTarget, context: ScriptContext): Promise<void> {
+  if (!isItem(this)) {
+    throw new ScriptTargetError('target must be an item');
+  }
+
   const actor = mustExist(context.actor);
 
   await context.state.show('item.use.any', { actor, item: this });

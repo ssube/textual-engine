dbb9addc47d29da88d7aca4b61a4da0c08f110b9
ssube <seansube@gmail.com>
2021-06-13 18:15:35 -0500

    fix(render): indicate which shortcut menu has focus (fixes #116)

--- src/component/ink/Shortcuts.tsx
+++ src/component/ink/Shortcuts.tsx
@@ -1,5 +1,5 @@
 import { doesExist, Optional } from '@apextoaster/js-utils';
-import { Box, useFocus } from 'ink';
+import { Box, Text, useFocus } from 'ink';
 import SelectInput from 'ink-select-input';
 import * as React from 'react';
 
@@ -12,21 +12,45 @@ export const Shortcuts = (props: ShortcutProps): JSX.Element => {
   const { isFocused: tabFocus } = useFocus();
   const { isFocused: itemFocus } = useFocus();
 
-  function selectItem(item: Optional<{label: string; value: string}>) {
+  function selectItem(item: Optional<{ label: string; value: string }>) {
     if (doesExist(item)) {
       props.onSelect(item.value);
     }
   }
 
-  return <Box flexDirection='column'>
-    <Box marginBottom={1}>
-      <SelectInput isFocused={tabFocus} items={SHORTCUT_TABS} onSelect={(it) => setTab(it.value as ShortcutKeys)} />
-    </Box>
-    <Box>
-      <SelectInput isFocused={itemFocus} items={props[tab].map((it) => ({
+  const items = props[tab].map((it) => ({
         label: it.name,
         value: it.id,
-      }))} onSelect={selectItem} />
+      }));
+
+  return <Box flexDirection='column'>
+    <Box marginBottom={1}>
+      <SelectInput
+        indicatorComponent={tabFocus ? FocusedIndicator : UnfocusedIndicator}
+        isFocused={tabFocus}
+        items={SHORTCUT_TABS}
+        onSelect={(it) => setTab(it.value as ShortcutKeys)}
+      />
+    </Box>
+    <Box>
+      <SelectInput
+        indicatorComponent={itemFocus ? FocusedIndicator : UnfocusedIndicator}
+        isFocused={itemFocus}
+        items={items}
+        onSelect={selectItem}
+      />
     </Box>
   </Box>;
 };
+
+export const FocusedIndicator = ({ isSelected = false }) => (
+  <Box marginRight={1}>
+    {isSelected ? <Text color="green">&gt;</Text> : <Text> </Text>}
+  </Box>
+);
+
+export const UnfocusedIndicator = ({ isSelected = false }) => (
+  <Box marginRight={1}>
+    {isSelected ? <Text color="blue">&gt;</Text> : <Text> </Text>}
+  </Box>
+);

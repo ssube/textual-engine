4e1708b8c4e3c4568310be2c1a60e93cd5b7c113
ssube <seansube@gmail.com>
2021-06-16 08:39:43 -0500

    fix(test): cover throttle helpers

---
+++ test/util/async/TestThrottle.ts
@@ -0,0 +1,71 @@
+import { doesExist, mustExist, Optional } from '@apextoaster/js-utils';
+import { expect } from 'chai';
+import { SinonFakeTimers, stub, useFakeTimers } from 'sinon';
+
+import { debounce, throttle } from '../../../src/lib';
+
+describe('throttle utils', () => {
+  let clock: Optional<SinonFakeTimers>;
+
+  before(() => {
+    clock = useFakeTimers();
+  });
+
+  after(() => {
+    if (doesExist(clock)) {
+      clock.restore();
+    }
+  });
+
+  describe('debounce helper', () => {
+    it('should throttle calls', async () => {
+      const inner = stub();
+      const slow = debounce(10, inner);
+
+      slow.call();
+      slow.call();
+      await mustExist(clock).tickAsync(20);
+
+      expect(inner).to.have.callCount(1);
+    });
+
+    it('should not call after being cleared', async () => {
+      const inner = stub();
+      const slow = debounce(10, inner);
+
+      slow.call();
+      slow.clear();
+      await mustExist(clock).tickAsync(20);
+
+      expect(inner).to.have.callCount(0);
+    });
+  });
+
+  describe('throttle helper', () => {
+    it('should throttle calls', async () => {
+      const inner = stub();
+      const slow = throttle(10, inner);
+
+      slow.call();
+      slow.call();
+      await mustExist(clock).tickAsync(20);
+
+      expect(inner).to.have.callCount(1);
+    });
+
+    it('should not call after being cleared', async () => {
+      const inner = stub();
+      const slow = throttle(10, inner);
+
+      slow.call(); // start throttling
+
+      expect(inner).to.have.callCount(1);
+
+      slow.call(); // should be throttled
+      slow.clear(); // cancels the throttled call
+      await mustExist(clock).tickAsync(20);
+
+      expect(inner).to.have.callCount(1);
+    });
+  });
+});

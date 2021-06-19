07fcd9a07c4eece91e346657de3c84a016118372
ssube <seansube@gmail.com>
2021-06-19 14:37:23 -0500

    feat(tokenizer): add rudimentary NLP tokenizer (#94)

--- src/service/tokenizer/NaturalTokenizer.ts
+++ src/service/tokenizer/NaturalTokenizer.ts
@@ -1,18 +1,39 @@
-import { NotImplementedError } from '@apextoaster/js-utils';
+import { getOrDefault, mustCoalesce } from '@apextoaster/js-utils';
+import nlp from 'compromise';
 
 import { TokenizerService } from '.';
-import { Command } from '../../model/Command';
+import { Command, makeCommand } from '../../model/Command';
+import { InjectedOptions } from '../../module';
+import { WordTokenizer } from './WordTokenizer';
 
-export class NaturalTokenizer implements TokenizerService {
-  public async parse(_input: string): Promise<Array<Command>> {
-    throw new NotImplementedError();
+export class NaturalTokenizer extends WordTokenizer implements TokenizerService {
+  protected nlp: typeof nlp;
+
+  constructor(options: InjectedOptions) {
+    super(options);
+
+    this.nlp = nlp;
   }
 
-  public async split(_input: string): Promise<Array<string>> {
-    throw new NotImplementedError();
-  }
+  public async parse(input: string): Promise<Array<Command>> {
+    const doc = this.nlp(input);
+    const nouns = doc.nouns().out('array');
+    const numbers = doc.termList().filter((it) => it.tags.Cardinal).map((it) => it.text);
+    const [rawIndex] = numbers;
 
-  public async translate(_verbs: ReadonlyArray<string>): Promise<void> {
-    throw new NotImplementedError();
+    const verbs = doc.verbs();
+    const others = verbs.trim().slice(1).out('array');
+    const rawVerb = verbs.toInfinitive().first().toLowerCase().text();
+    this.logger.debug({ nouns, others, rawIndex, rawVerb }, 'parsed parts of speech from document');
+
+    const index = parseInt(mustCoalesce(rawIndex, '0'), 10);
+    const target = [...nouns, ...others].join(' ');
+    const verb = getOrDefault(this.verbs, rawVerb, rawVerb);
+    const command = makeCommand(verb, target, index);
+
+    const terms = doc.termList();
+    this.logger.debug({ command, terms }, 'built command from document');
+
+    return [command];
   }
 }

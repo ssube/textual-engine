b862f2ebafde98193ed7bc6d222edf401346b306
ssube <seansube@gmail.com>
2021-07-04 23:19:08 -0500

    fix(test): cover Ink shortcuts within frame

--- test/component/ink/TestFrame.ts
+++ test/component/ink/TestFrame.ts
@@ -5,7 +5,7 @@ import { stub } from 'sinon';
 
 import { Frame } from '../../../src/component/ink/Frame';
 import { zeroStep } from '../../../src/util/entity';
-import { KEY_ENTER, KEY_TAB } from '../../constants';
+import { KEY_ARROW_DOWN, KEY_ENTER, KEY_TAB } from '../../constants';
 import { sendKeys } from './helper';
 
 describe('ink frame component', () => {
@@ -87,7 +87,7 @@ describe('ink frame component', () => {
     const { lastFrame, stdin } = render(root);
 
     await sendKeys(stdin, 10, [
-      KEY_TAB,        // initial
+      KEY_TAB,        // input
       'a',
       'b',
       KEY_ENTER,      // submit
@@ -96,5 +96,102 @@ describe('ink frame component', () => {
     lastFrame(); // render and stuff
 
     expect(onLine).to.have.callCount(1).and.been.calledWith('ab');
+  });
+
+  it('should append entity shortcuts to the line', async () => {
+    const onLine = stub();
+    const root = React.createElement(Frame, {
+      onLine,
+      prompt: 'foo',
+      output: [],
+      quit: false,
+      shortcuts: {
+        actors: [{
+          id: 'c',
+          name: 'C',
+        }],
+        items: [{
+          id: 'd',
+          name: 'D',
+        }],
+        portals: [],
+        verbs: [],
+      },
+      show: {
+        shortcuts: true,
+        status: true,
+      },
+      stats: [],
+      step: zeroStep(),
+    });
+
+    const { lastFrame, stdin } = render(root);
+
+    await sendKeys(stdin, 10, [
+      KEY_TAB,        // input
+      'a',
+      'b',
+      KEY_TAB,        // tabs
+      KEY_TAB,        // shortcuts
+      KEY_ENTER,      // append actor
+      KEY_TAB,        // cycle through input
+      KEY_TAB,        // back to tabs
+      KEY_ARROW_DOWN, // items tab
+      KEY_ENTER,      // select tab
+      KEY_TAB,        // shortcuts
+      KEY_ENTER,      // append item
+      KEY_TAB,        // back to input
+      KEY_ENTER,      // submit
+    ]);
+
+    lastFrame(); // render and stuff
+
+    expect(onLine).to.have.been.calledWith('ab c d');
+  });
+
+  it('should replace the line with verb shortcuts', async () => {
+    const onLine = stub();
+    const root = React.createElement(Frame, {
+      onLine,
+      prompt: 'foo',
+      output: [],
+      quit: false,
+      shortcuts: {
+        actors: [],
+        items: [],
+        portals: [],
+        verbs: [{
+          id: 'c',
+          name: 'C',
+        }],
+      },
+      show: {
+        shortcuts: true,
+        status: true,
+      },
+      stats: [],
+      step: zeroStep(),
+    });
+
+    const { lastFrame, stdin } = render(root);
+
+    await sendKeys(stdin, 10, [
+      KEY_TAB,        // input
+      'a',
+      'b',
+      KEY_TAB,        // tabs
+      KEY_ARROW_DOWN, // items tab
+      KEY_ARROW_DOWN, // portals tab
+      KEY_ARROW_DOWN, // verbs tab
+      KEY_ENTER,      // select tab
+      KEY_TAB,        // shortcuts
+      KEY_ENTER,      // append verb
+      KEY_TAB,        // back to input
+      KEY_ENTER,      // submit
+    ]);
+
+    lastFrame(); // render and stuff
+
+    expect(onLine).to.have.been.calledWith('c');
   });
 });

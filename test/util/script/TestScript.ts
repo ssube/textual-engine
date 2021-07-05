01531413dcc537db84711446da7a472441884969
ssube <seansube@gmail.com>
2021-07-05 00:24:53 -0500

    test removal of empty verb scripts

--- test/util/script/TestScript.ts
+++ test/util/script/TestScript.ts
@@ -1,11 +1,37 @@
 import { expect } from 'chai';
+import { getVerbScripts } from '../../../src/lib';
+import { makeTestActor, makeTestRoom } from '../../entity';
 
 describe('script utils', () => {
   describe('verb script helper', () => {
+    it('should remove scripts with an empty name', async () => {
+      const actor = makeTestActor('', '', '');
+      actor.scripts.set('verbs.foo', {
+        data: new Map(),
+        name: 'foo',
+      });
+      actor.scripts.set('verbs.bar', {
+        data: new Map(),
+        name: 'bar',
+      });
+
+      const room = makeTestRoom('', '', '');
+      room.scripts.set('verbs.bar', {
+        data: new Map(),
+        name: '',
+      });
+
+      const verbs = getVerbScripts({
+        actor,
+        room,
+      });
+
+      expect(verbs).to.be.a('map').and.to.have.lengthOf(1); // only foo, bar should be removed
+    });
+
     xit('should get scripts from the target entity');
     xit('should get scripts from items within the target entity');
     xit('should get scripts from the room containing the target entity');
     xit('should replace actor scripts with room scripts');
-    xit('should remove scripts with an empty name');
   });
 });
